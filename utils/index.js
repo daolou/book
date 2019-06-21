df4ae1f91e0a66ed1ee6a81059120e331cef1c93
Mr-jiangzhiguo <jiangzhiguo2010@qq.com>
2019-06-21 12:25:12 +0800

    workflow(utils): add getValue method

--- utils/index.js
+++ utils/index.js
@@ -37,6 +37,39 @@ export const getDataType = o => {
 };
 
 /**
+ * @description 根据路径获取对象的值（也可用插件@babel/plugin-proposal-optional-chaining）
+ * @param {object} obj - 对象
+ * @param {string} keypath - 路径
+ * @param {any} defaultValue - 默认值
+ */
+export const getValue = (obj, keypath, defaultValue = undefined) => {
+  if (!obj) {
+    throw new Error('the first param obj is required');
+  }
+  if (!keypath) {
+    throw new Error('the second param keypath is required');
+  }
+  if (getDataType(obj) !== 'Object') {
+    throw new Error('the first param obj must be object');
+  }
+  if (getDataType(keypath) !== 'String') {
+    throw new Error('the second param keypath must be string');
+  }
+  // return String.prototype.split.call(keypath, /[,[\].]+?/)
+  //   .filter(Boolean)
+  //   .reduce((a, c) => (Object.hasOwnProperty.call(a,c) ? a[c] : defaultValue), obj)
+
+  keypath = keypath.replace(/\[(\d+)\]/g, '.$1').split('.');
+  for (const key of keypath) {
+    obj = Object(obj)[key];
+    if (obj === undefined) {
+      return defaultValue;
+    }
+  }
+  return obj;
+};
+
+/**
  * @description 获取某个区间[min=0, max]的随机数
  * @param {number} max - 区间最大值
  * @param {number} min - 区间最小值,默认为0
