30ebf028c23f4afb7db30cfb0628a1c22af62be7
Mr-jiangzhiguo <jiangzhiguo2010@qq.com>
2019-07-16 11:31:51 +0800

    workflow(share): artile async/await done

--- utils/gen.js
+++ utils/gen.js
@@ -10,42 +10,71 @@ const gen = function*() {
   console.log(res2);
 };
 
-const g = gen();
+const run1 = gen => {
+  const g = gen();
 
-const g1 = g.next();
-console.log('g1:', g1);
+  const g1 = g.next();
+  console.log('g1:', g1);
 
-g1.value
-  .then(res1 => {
-    console.log('res1:', res1);
-    const g2 = g.next(res1);
-    console.log('g2:', g2);
-    g2.value
-      .then(res2 => {
-        console.log('res2:', res2);
-        g.next(res2);
-      })
-      .catch(err2 => {
-        console.log(err2);
-      });
-  })
-  .catch(err1 => {
-    console.log(err1);
-  });
-// g1: { value: Promise { <pending> }, done: false }
-// res1: {
-//   "a": 1
-// }
+  g1.value
+    .then(res1 => {
+      console.log('res1:', res1);
+      const g2 = g.next(res1);
+      console.log('g2:', g2);
+      g2.value
+        .then(res2 => {
+          console.log('res2:', res2);
+          g.next(res2);
+        })
+        .catch(err2 => {
+          console.log(err2);
+        });
+    })
+    .catch(err1 => {
+      console.log(err1);
+    });
+  // g1: { value: Promise { <pending> }, done: false }
+  // res1: {
+  //   "a": 1
+  // }
 
-// {
-//   "a": 1
-// }
+  // {
+  //   "a": 1
+  // }
 
-// g2: { value: Promise { <pending> }, done: false }
-// res2: {
-//   "b": 2
-// }
+  // g2: { value: Promise { <pending> }, done: false }
+  // res2: {
+  //   "b": 2
+  // }
 
-// {
-//   "b": 2
-// }
+  // {
+  //   "b": 2
+  // }
+};
+
+const run2 = gen => {
+  const g = gen();
+  function next(data) {
+    const res = g.next(data);
+    if (res.done) return res.value;
+    res.value.then(function(data) {
+      next(data);
+    });
+  }
+  next();
+  // {
+  //   "a": 1
+  // }
+
+  // {
+  //   "b": 2
+  // }
+};
+
+// run1(gen);
+run2(function*() {
+  const res1 = yield readFile(path.resolve(__dirname, '../data/a.json'), { encoding: 'utf8' });
+  console.log(res1);
+  const res2 = yield readFile(path.resolve(__dirname, '../data/b.json'), { encoding: 'utf8' });
+  console.log(res2);
+});
