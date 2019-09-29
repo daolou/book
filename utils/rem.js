0c73149d22fb2363437f009a003a1533b9100430
Mr-jiangzhiguo <jiangzhiguo2010@qq.com>
2019-09-29 17:23:47 +0800

    workflow(update): share/08 add npm link test

--- utils/rem.js
+++ utils/rem.js
@@ -1,6 +1,7 @@
 /**
  * @description （多屏幕+手机系统字体+webview+750设计稿）适配方案
  * 1rem = 100px
+ * [⚠️]：更改viewport会影响到iframe，若有使用iframe请忽略此方案，可以尝试用https://github.com/evrone/postcss-px-to-viewport
  */
 (function(win, doc) {
   var docEl = doc.documentElement;
@@ -9,13 +10,27 @@
   var _scale = 1 / _dpr;
 
   var setStyleFontSize = function(num) {
-    docEl.style.fontSize = (num > 100 ? 100 : num) + 'px';
+    var finalRem = num > 50 * _dpr ? 50 * _dpr : num;
+    docEl.style.fontSize = finalRem + 'px';
+    /* 给js调用的，某一dpr下rem和px之间的转换函数 */
+    win.rem2px = function(v) {
+      v = parseFloat(v);
+      return v * finalRem;
+    };
+    win.px2rem = function(v) {
+      v = parseFloat(v);
+      return v / finalRem;
+    };
+
+    win.dpr = _dpr;
+    win.rem = finalRem;
   };
   // 计算并设置根元素fontSize
   var calcRem = function() {
     var _rem = (docEl.clientWidth * _dpr) / 7.5;
 
     /* 设置viewport，进行缩放，达到高清效果 */
+    // 此后获取宽度（clientWidth）无需在乘以 dpr
     metaEl.setAttribute(
       'content',
       'width=device-width' +
@@ -32,19 +47,6 @@
 
     /* 动态写入样式 */
     setStyleFontSize(_rem);
-
-    /* 给js调用的，某一dpr下rem和px之间的转换函数 */
-    win.rem2px = function(v) {
-      v = parseFloat(v);
-      return v * _rem;
-    };
-    win.px2rem = function(v) {
-      v = parseFloat(v);
-      return v / _rem;
-    };
-
-    win.dpr = _dpr;
-    win.rem = _rem;
   };
 
   /* 解决部分手机webview一开始获取的clientWidth为0，导致font-size为0即1rem=0 的bug */
