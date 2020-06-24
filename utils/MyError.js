de80e77ad4be71768e516d8aa5d68af49844058c
Mr-jiangzhiguo <jiangzhiguo2010@qq.com>
2020-06-24 12:05:57 +0800

    fix(utils): update MyError

--- utils/MyError.js
+++ utils/MyError.js
@@ -1,10 +1,30 @@
 /**
- * @description 扩展Error
+ * @description 集中式错误工厂，兼容 node 端和 浏览端（es6+、es5）
+ * Error 扩展
  */
-export class MyError extends Error {
-  constructor(message, cause) {
+export class BaseError extends Error {
+  constructor(message) {
     super(message);
-    this.name = this.constructor.name;
-    this.cause = cause;
+    this.name = new.target.name;
+
+    if (typeof Error.captureStackTrace === 'function') {
+      // v8 引擎暴露的 api，nodejs 可用
+      // https://devdocs.io/node/errors#errors_class_error
+      Error.captureStackTrace(this, new.target);
+    }
+
+    if (typeof Object.setPrototypeOf === 'function') {
+      // es6+
+      Object.setPrototypeOf(this, new.target.prototype);
+    } else {
+      // es5
+      this.__proto__ = new.target.prototype;
+    }
   }
 }
+
+export class NodeError extends BaseError {}
+
+export class WebError extends BaseError {}
+
+export class ResponseError extends BaseError {}
